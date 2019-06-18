2994184c11de0cb90836c1bf17edc75f55cec705
qixinbing <qixinbing@rongcloud.cn>
2019-06-18 11:53:10 +0800

    问题:增加注释 原因: 解决方案: Reviewer: 工单号: Log: X

--- lib/message.dart
+++ lib/message.dart
@@ -1,17 +1,17 @@
 import 'message_content.dart';
 
 class Message extends Object {
-  int conversationType;
-  String targetId;
-  int messageId;
-  int messageDirection;
-  String senderUserId;
-  int receivedStatus;
-  int sentStatus;
-  int sentTime;
-  String objectName;
-  MessageContent content;
-  String messageUId;
+  int conversationType;//会话类型 参见 RCConversationType
+  String targetId;//会话 id
+  int messageId;//messageId ，本地数据库的自增 id
+  int messageDirection;//消息方向 参见 RCMessageDirection
+  String senderUserId;//发送者 id
+  int receivedStatus;//消息接收状态 参见 RCReceivedStatus
+  int sentStatus;//消息发送状态 参见 RCSentStatus
+  int sentTime;//发送时间，unix 时间戳，单位毫秒
+  String objectName;//消息 objName
+  MessageContent content;//消息内容
+  String messageUId;//消息 UID，全网唯一 Id
   
   //如果 content 为 null ，说明消息内容本身未被 flutter 层正确解析，则消息内容会保存到该 map 中
   Map originContentMap;
--- lib/rc_status_define.dart
+++ lib/rc_status_define.dart
@@ -7,10 +7,26 @@ class RCMessagePersistentFlag {
 }
 
 //消息发送状态
-class RCMessageSentStatus {
+class RCSentStatus {
   static const int Sending = 10;//发送中
   static const int Failed = 20;//发送失败
   static const int Sent = 30;//发送成功
+}
+
+//消息方向
+class RCMessageDirection {
+  static const int Send = 1;
+  static const int Receive = 2;
+}
+
+//消息接收状态
+class RCReceivedStatus {
+  static const int Unread = 0;//未读
+  static const int Read = 1;//已读
+  static const int Listened = 2;//已听，语音消息
+  static const int Downloaded = 4;//已下载
+  static const int Retrieved = 8;//已经被其他登录的多端收取过
+  static const int MultipleReceive = 16;//被多端同时收取
 }
 
 //回调状态
