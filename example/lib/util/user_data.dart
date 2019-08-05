be89860a1772cf59f257ea9d208a4f8c21d8f56e
qixinbing <qixinbing@rongcloud.cn>
2019-08-05 12:00:56 +0800

    问题:剔除无用信息 原因: 解决方案: Reviewer: 工单号: Log: X

--- example/lib/util/user_data.dart
+++ example/lib/util/user_data.dart
@@ -2,11 +2,8 @@
 
 //融云 appkey
 String RongAppKey = 'pvxdm17jxjaor';
-//zyf
-// String RongAppKey = '82hegw5u8xx1x';
 //用户 id
-String CurrentUserId = 'android';
+String CurrentUserId = 'android'; 
 
 //通过用户 id 生成的对应融云 token
 String RongIMToken = '/GAO1QE3NeKdxZ8EFWZnXSBpWcymqs7mr0LfCBn63cWXAWvMuW73BKKASyaZmGFGhVYuRiYRxSacloyurITSuw==';
-// String RongIMToken = '8rH05FfJ18Yi61X3MLmzqsvdXplDZYquU3d6wHEoK21q/GcAuQKnIsBlxXuFsJinBYNv5pIqOgl2fcEikz00AQ==';
