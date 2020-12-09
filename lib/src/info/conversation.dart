f69b41b62b7d196a622e3d49fe698205bbdbdf40
SunHao <sunhao@rongcloud.cn>
2020-12-09 17:27:29 +0800

    Revert "feat：新增 Conversation 属性"
    This reverts commit 4089ae66b155f759d53e031e82c1d108cd7f1a7b.

--- lib/src/info/conversation.dart
+++ lib/src/info/conversation.dart
@@ -14,11 +14,6 @@ class Conversation {
   MessageContent latestMessageContent;
   int mentionedCount; // 会话中@消息的个数
   String draft; //会话草稿内容
-  int blockStatus; // 会话是否是免打扰状态
-  int receivedTime;
-  int lastestMessageDirection; //会话中最后一条消息的方向
-  String lastestMessageUId; //最后一条消息的全局唯一 ID
-  bool hasUnreadMentioned; //会话中是否存在被 @ 的消息
 
   //如果 content 为 null ，说明消息内容本身未被 flutter 层正确解析，则消息内容会保存到该 map 中
   Map originContentMap;
