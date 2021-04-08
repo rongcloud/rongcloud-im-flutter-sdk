6c753fc28faed1d4c68a29f41dcb67dda94b55d1
qixinbing <qixinbing@rongcloud.cn>
2021-04-08 15:03:39 +0800

    问题:RongcloudImPlugin 支持 flutter 2.0 原因及方案: Reviewer: 来源:

--- android/src/main/java/io/rong/flutter/imlib/RongcloudImPlugin.java
+++ android/src/main/java/io/rong/flutter/imlib/RongcloudImPlugin.java
@@ -1,24 +1,33 @@
 package io.rong.flutter.imlib;
 
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** RongcloudImPlugin */
-public class RongcloudImPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "rongcloud_im_plugin");
-    channel.setMethodCallHandler(new RongcloudImPlugin());
-    RCIMFlutterWrapper.getInstance().saveContext(registrar.activity().getApplicationContext());
-    RCIMFlutterWrapper.getInstance().saveChannel(channel);
-  }
+public class RongcloudImPlugin implements FlutterPlugin,MethodCallHandler {
+
+  private MethodChannel channel;
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     RCIMFlutterWrapper.getInstance().onFlutterMethodCall(call,result);
   }
 
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    channel = new MethodChannel(binding.getBinaryMessenger(),"rongcloud_im_plugin");
+    channel.setMethodCallHandler(this);
+    RCIMFlutterWrapper.getInstance().saveContext(binding.getApplicationContext());
+    RCIMFlutterWrapper.getInstance().saveChannel(channel);
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
 }
