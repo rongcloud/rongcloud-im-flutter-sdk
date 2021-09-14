e87811aa647eabc741258a24ded1a21cc9f3e7dc
SunHao <sunhao@rongcloud.cn>
2021-09-14 15:22:44 +0800

    feat：适配 Android embedding v2.

--- example/android/app/src/main/java/com/example/rongcloud_im_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/com/example/rongcloud_im_plugin_example/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 
 
-import io.flutter.app.FlutterActivity;
+import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
@@ -30,4 +30,9 @@ public class MainActivity extends FlutterActivity {
 //      }
 //    },500);
     }
+
+    @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
 }
