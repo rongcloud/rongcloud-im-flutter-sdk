3bb1303bc7bad59dee2a619e30290f9ef12874a8
GP-Moon <pmgd19881226@gmail.com>
2021-09-18 09:58:40 +0800

    fix: 修复android编译bug

--- android/src/main/java/io/rong/flutter/imlib/RongcloudImPlugin.java
+++ android/src/main/java/io/rong/flutter/imlib/RongcloudImPlugin.java
@@ -4,42 +4,27 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** RongcloudImPlugin */
-public class RongcloudImPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "rongcloud_im_plugin");
-    channel.setMethodCallHandler(new RongcloudImPlugin());
-    RCIMFlutterWrapper.getInstance().saveContext(registrar.activity().getApplicationContext());
-    RCIMFlutterWrapper.getInstance().saveChannel(channel);
-  }
+public class RongcloudImPlugin implements FlutterPlugin, MethodChannel.MethodCallHandler {
 
-  // @Override
-  // public void onMethodCall(MethodCall call, Result result) {
-    // RCIMFlutterWrapper.getInstance().onFlutterMethodCall(call,result);
-  // }
-
-  @Override
-  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      Log.e("onMethodCall", call.method);
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-      Map<String, String> map = new HashMap<>();
-      map.put("message", "message");
-      // channel.invokeMethod("onMessageTest", map);
-      RCIMFlutterWrapper.getInstance().onFlutterMethodCall(call,result);
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        final MethodChannel channel = new MethodChannel(binding.getBinaryMessenger(), "rongcloud_im_plugin");
+        channel.setMethodCallHandler(this);
+        RCIMFlutterWrapper.getInstance().saveContext(binding.getApplicationContext());
+        RCIMFlutterWrapper.getInstance().saveChannel(channel);
     }
-  }
 
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        Log.e("onDetachedFromEngine", "onDetachedFromEngine");
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        RCIMFlutterWrapper.getInstance().onFlutterMethodCall(call, result);
+    }
 }
--- example/android/app/src/main/java/com/example/rongcloud_im_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/com/example/rongcloud_im_plugin_example/MainActivity.java
@@ -1,41 +1,46 @@
 package com.example.rongcloud_im_plugin_example;
 
-import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 
 import com.example.rongcloud_im_plugin_example.message.LocationMessage;
 
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.rong.flutter.imlib.RCIMFlutterWrapper;
 
 public class MainActivity extends FlutterActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        GeneratedPluginRegistrant.registerWith(this);
-        Context con = getApplicationContext();
         RCIMFlutterWrapper.getInstance().registerMessage(LocationMessage.class);
-        //Android 注册自定义消息必须在 init 之后
-        //如果注册了自定义消息，但是没有注册消息模板，无法进入 SDK 的聊天页面
-        //https://www.rongcloud.cn/docs/android.html 参见文档的"消息自定义"
-//    RongIMClient.init(con,"pvxdm17jxjaor");
-//    RCIMFlutterWrapper.getInstance().registerMessage(TestMessage.class);
-
-        // 测试Android往Flutter传递数据
-//    Timer timer = new Timer();
-//    timer.schedule(new TimerTask() {
-//      public void run() {
-//        Map map = new HashMap();
-//        map.put("key","android");
-//        RCIMFlutterWrapper.getInstance().sendDataToFlutter(map);
-//      }
-//    },500);
     }
 
-    @Override
-  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+    //    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        GeneratedPluginRegistrant.registerWith(this);
+//        Context con = getApplicationContext();
+//        RCIMFlutterWrapper.getInstance().registerMessage(LocationMessage.class);
+//        //Android 注册自定义消息必须在 init 之后
+//        //如果注册了自定义消息，但是没有注册消息模板，无法进入 SDK 的聊天页面
+//        //https://www.rongcloud.cn/docs/android.html 参见文档的"消息自定义"
+////    RongIMClient.init(con,"pvxdm17jxjaor");
+////    RCIMFlutterWrapper.getInstance().registerMessage(TestMessage.class);
+//
+//        // 测试Android往Flutter传递数据
+////    Timer timer = new Timer();
+////    timer.schedule(new TimerTask() {
+////      public void run() {
+////        Map map = new HashMap();
+////        map.put("key","android");
+////        RCIMFlutterWrapper.getInstance().sendDataToFlutter(map);
+////      }
+////    },500);
+//    }
+//
+//    @Override
+//    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//    }
 }
