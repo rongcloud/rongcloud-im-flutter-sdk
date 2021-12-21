d1e8cd8b124831c60d8db9873becc1f8dbf94d8a
panmingda <panmingda@rongcloud.cn>
2021-12-21 18:49:44 +0800

    fix: 修改默认用户token

--- example/lib/user_data.dart
+++ example/lib/user_data.dart
@@ -1,8 +1,8 @@
 //融云 appkey
-String RongAppKey = 'z3v5yqkbv8v30';
+String RongAppKey = 'n19jmcy59f1q9';
 
 //用户 id
-String CurrentUserId = 'ftd_1639559452953';
+String CurrentUserId = 'vnVFC3hnE';
 
 //通过用户 id 生成的对应融云 token
-String RongIMToken = 'Y5w0rF/AzBk/fGEj4oGTMhrnqWWiU1eSq7+si4yGEz4PZlPFeotWlw==@emx6.cn.rongnav.com;emx6.cn.rongcfg.com';
+String RongIMToken = 'dsvXZKrfCO/OA1PS5qxbZOF+lwb3rejuvSXKtbO03G0=@h4mx.cn.rongnav.com;h4mx.cn.rongcfg.com';
