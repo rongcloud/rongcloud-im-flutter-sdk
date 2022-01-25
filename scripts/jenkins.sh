85993bcaa0612f85383858023b7d694f0c0ee78e
zhangyifan <zhangyifan@rongcloud.cn>
2022-01-25 18:04:01 +0800

    chore: 修改构建脚本

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
+cd $(dirname "$0") && pwd;
+
 security unlock-keychain -p 5173rongcloud ~/Library/Keychains/login.keychain-db
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/login.keychain-db
 
-bash release.sh -p "$PLATFORM" \
+sh release.sh -p "$PLATFORM" \
