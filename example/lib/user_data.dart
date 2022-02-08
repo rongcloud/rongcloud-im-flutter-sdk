145ec77088eb0d1bb597168e7beb2ac0dd68358c
zhangyifan <zhangyifan@rongcloud.cn>
2022-02-08 09:57:40 +0800

    fix: 添加appkey 和 token

--- example/lib/user_data.dart
+++ example/lib/user_data.dart
@@ -1,8 +1,9 @@
 //融云 appkey
-String RongAppKey = '';
+// String RongAppKey = 'n19jmcy59f1q9';
+String RongAppKey = 'c9kqb3rdkbb8j';
 
 //用户 id
-String CurrentUserId = '';
+String CurrentUserId = 'vnVFC3hnE';
 
 //通过用户 id 生成的对应融云 token  3 Peg9I35Q8
-String RongIMToken = '';
+String RongIMToken = 'SLNh49yERZF/lN87UBMj8G33zkQsbE8eVSFdUMVanNuoIdDe7fczf+SEo2ow0HJY2jV+Ub4/pqyIuEBzsIgcWzwM8cj754Iz';
